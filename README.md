a979ac5cd891eac5d6b0f3d6b6faad2d7c6d31bc
ferares <fermin@ares.uy>
2019-04-27 14:57:05 -0300

    remove PHP code for a 100% node solution

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Installation
 
 1. `npm install`
-2. `composer install`
 
 # Configuration
+
+  This configuration is optional, if no configuration is provided a test SMTP service account from [ethereal.email](https://ethereal.email/) will be used.
 
 1. Make a copy of `config.json.example` and name it `config.json`
 2. Write your personal configuration into `config.json`
 
 # Usage
 
-1. Write your code in `src/mailing.html` and your styles in `src/styles.css`
-2. Run `npm start` to inline your styles in the HTML
-3. Run `php index.php` to send the HTML email
+1. Write your code in `src/index.html` and your styles in `src/styles.css` (reference the stylesheet from within the HTML using a <link> tag)
+2. Run `npm start` to inline your styles in the HTML and output the result to the `build` folder
+3. Run `npm run email` to inline your styles in the HTML, output the result to the `build` folder and send the HTML email
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,65 @@
-const gulp      = require('gulp');
-const inlineCss = require('gulp-inline-css');
+const gulp       = require('gulp');
+const inlineCss  = require('gulp-inline-css');
+const nodemailer = require('nodemailer');
+const fs         = require('fs');
 
-const html = 'src/mailing.html';
+const html = 'src/index.html';
 
-gulp.task('default', () =>
+function build(done) {
   gulp.src(html)
   .pipe(inlineCss())
-  .pipe(gulp.dest('build/'))
-);
+  .pipe(gulp.dest('build/'));
+  done();
+};
+
+
+function sendMail(config, done) {
+  const nodemailerConfig = {
+    host: config.host || 'smtp.ethereal.email',
+    port: config.port || 587,
+    secure: config.secure || false,
+    auth: {
+      user: config.user,
+      pass: config.pass,
+    },
+  };
+
+  let recipients = config.recipients || 'bar@example.com';
+
+  const transporter = nodemailer.createTransport(nodemailerConfig);
+
+  fs.readFile(html, 'utf8', (err, data) => {
+    if (err) throw err;
+
+    transporter.sendMail({
+      from: '"Mailing tester" <noreply@mailing.test>',
+      to: recipients,
+      subject: 'Mailing test',
+      text: 'You need an HTML compatible email client to read this email',
+      html: data,
+    }).then((info) => {
+      console.log('Message sent');
+      url = nodemailer.getTestMessageUrl(info);
+      if (url) {
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      }
+
+      done();
+    });
+  });
+}
+
+function email(done) {
+  let config = undefined;
+  try {
+    config = require('./config.json');
+    sendMail(config, done);
+  } catch (e) {
+    console.log('No config file found, using Etheral test SMTP service');
+    config = nodemailer.createTestAccount().then((config) => sendMail(config, done));
+  }
+};
+
+exports.default = build;
+
+exports.send = gulp.series(build, email);
