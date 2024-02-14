5502aea443e273cda0c0ced04dff4f5c37f769a8
ferares <fermin@ares.uy>
2024-02-14 16:07:07 -0300

    Replace inline-css with juice Update nodemailer Switch from npm to pnpm

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer');
-const inlineCss = require('inline-css');
+const juice = require('juice');
 const fs = require('fs');
 
 let config = '';
@@ -11,37 +11,36 @@ try {
 
 function sendMail(config) {
 
-  fs.readFile('./src/index.html', 'utf8', (err, html) => {
+  fs.readFile('./src/index.html', 'utf8', (err, data) => {
     if (err) throw err;
 
-    inlineCss(html, { url: `file://${__dirname}/src/` }).then((data) => {
-      const nodemailerConfig = {
-        host: config.host || 'smtp.ethereal.email',
-        port: config.port || 587,
-        secure: config.secure || false,
-        auth: {
-          user: config.user,
-          pass: config.pass,
-        },
-      };
+    const html = juice(data, { url: `file://${__dirname}/src/` })
 
-      const recipients = config.recipients || 'bar@example.com';
-      const transporter = nodemailer.createTransport(nodemailerConfig);
-      transporter.sendMail({
-        from: config.from || '"Mailing tester" <noreply@mailing.test>',
-        to: recipients,
-        subject: config.subject || 'Mailing test',
-        text: 'You need an HTML compatible email client to read this email',
-        html: data,
-      }).then((info) => {
-        console.log('Message sent');
-        url = nodemailer.getTestMessageUrl(info);
-        if (url) {
-          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        }
-      });
+    const nodemailerConfig = {
+      host: config.host || 'smtp.ethereal.email',
+      port: config.port || 587,
+      secure: config.secure || false,
+      auth: {
+        user: config.user,
+        pass: config.pass,
+      },
+    };
+
+    const recipients = config.recipients || 'bar@example.com';
+    const transporter = nodemailer.createTransport(nodemailerConfig);
+    transporter.sendMail({
+      from: config.from || '"Mailing tester" <noreply@mailing.test>',
+      to: recipients,
+      subject: config.subject || 'Mailing test',
+      text: 'You need an HTML compatible email client to read this email',
+      html: html,
+    }).then((info) => {
+      console.log('Message sent');
+      url = nodemailer.getTestMessageUrl(info);
+      if (url) {
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      }
     });
-
   });
 }
 
